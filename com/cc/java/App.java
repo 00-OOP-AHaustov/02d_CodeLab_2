bda938f272a4b61cdba8ed3ca9dc134406e12e1c
Anton Haustov <anton.haust@gmail.com>
2023-07-28 14:42:27 +0200

    Challenge Solution

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,12 +3,14 @@ package com.cc.java;
 public class App {
 
 	public static void main(String[] args) {
-	
-	
+		Mitarbeiter[] office = { new Mitarbeiter("Pan", "Peter", "CEO", 2010),
+				new Mitarbeiter("Django", "null", "Headhunter", 2012),
+				new Mitarbeiter("Deville", "Cruella", "Marketing", 2015) };
 
+				output(office[2].getInfo("familyName"));
+				output(office[1].getInfo("role"));
+				output(office[0].getInfo("position"));
 	}
-
-	
 
 	private static void output(String outStr) {
 		System.out.println(outStr);
---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,30 @@
+package com.cc.java;
+
+public class Mitarbeiter {
+    private String familyName;
+    private String firstName;
+    private String role;
+    private int yearOfEntry;
+
+    public Mitarbeiter(String familyName, String firstName, String role, int yearOfEntry) {
+        this.familyName = familyName;
+        this.firstName = firstName;
+        this.role = role;
+        this.yearOfEntry = yearOfEntry;
+    }
+
+    public String getInfo(String value) {
+        switch (value) {
+            case "firstName":
+                return this.firstName;
+            case "role":
+                return this.role;
+            case "familyName":
+                return this.familyName;
+            case "yearOfEntry":
+                return String.valueOf(yearOfEntry);
+            default:
+                return "Unknown value.";
+        }
+    }
+}
